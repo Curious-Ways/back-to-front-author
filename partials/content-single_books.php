4fe2e87d265f36f5c1d07c99218ea31fccfea91f
David Flindall <david@backtofront.london>
2016-11-12 11:03:38 +0000

    Removed inline styles from single book page

--- partials/content-single_books.php
+++ partials/content-single_books.php
@@ -15,7 +15,6 @@
 
 			<div class="publications-intro">
 
-
 				<?php 
 
 				// Title
@@ -34,149 +33,141 @@
 
 				</div>
 
-			
+				<div class="publication-cover">
 
-			<div style="width: 45%; float:left">
+					<?php 
 
-			<?php 
+					// Book Cover
+					// -----------------
 
-			// Book Cover
-			// -----------------
+					if ( has_post_thumbnail() ) { ?>
 
-			if ( has_post_thumbnail() ) { ?>
+						<div class="publication-image">
+							<?php the_post_thumbnail('w250');?>
+						</div>
 
-				<div class="publication-image">
-					<?php the_post_thumbnail('w250');?>
+					<?php } ?>	
+
+				</div>			
+
+				<div class="publication-details">
+
+					<ul class="list-plain" style="margin-top: 0;">
+
+						<?php 
+
+						// Date
+						// --------------
+
+						if (get_field('published_date')) {
+							echo '<li><strong>Published</strong>: '.get_field('published_date').'</li>';
+						}		
+
+						// Publisher name / website
+						// --------------------------
+
+						$publisher_name = get_field('publisher_name'); 
+
+						if (get_field('publisher_website')) {
+							
+							echo '<li><strong>Publisher</strong>: <a target="_blank" href="'.get_field('publisher_website').'">';
+								echo $publisher_name->name;
+							echo '</a></li>';
+
+						}	else {
+
+							echo '<li><strong>Published by</strong>: '. $publisher_name->name .'</li>';
+
+						}
+
+						// Book type
+						// ------------------
+
+						/* $terms = get_field('book_type');
+
+						if( $terms ): $numItems = count($terms); $i = 0; ?>
+
+							<li><strong>Book Type: </strong>
+
+							<?php foreach( $terms as $term ): ?>
+
+								<?php echo $term->name; if (++$i != $numItems) {echo ', ';}?>
+							
+							<?php endforeach; ?>
+
+							</li>
+
+						<?php endif; */						
+
+						// Book format
+						// ------------------
+
+						$book_format_terms = get_field('book_format');
+
+						if( $book_format_terms ): $numItems = count($book_format_terms); $i = 0; ?>
+
+							<li><strong>Book Format: </strong>
+
+							<?php foreach( $book_format_terms as $term ): ?>
+
+								<?php echo $term->name; if (++$i != $numItems) {echo ', ';}?>
+							
+							<?php endforeach; ?>
+
+							</li>
+
+						<?php endif;	
+
+						// RRP
+						// ------------------
+
+						if (get_field('rrp')) {
+							echo '<li><strong>RRP</strong>: '.get_field('rrp').'</li>';
+						}		
+
+						// ISBN numbers
+						// ------------------
+
+						if( have_rows('isbn_numbers') ):  while ( have_rows('isbn_numbers') ) : the_row(); ?>
+
+							<li><strong><?php the_sub_field('isbn_label'); ?></strong>: <?php the_sub_field('isbn_number'); ?></li>
+
+						<?php endwhile; 
+
+						else :
+
+						// no rows found
+
+						endif; ?>
+
+					</ul>
+
+					<?php 
+
+					// Sales links
+					// ------------------
+
+					if( have_rows('sales_links') ): ?>
+
+						<h3>Available at these locations</h3>
+
+						<ul class="list-plain">
+
+					  	<?php while ( have_rows('sales_links') ) : the_row(); ?>
+
+					    	<li><a target="_blank" href="<?php the_sub_field('retailer_link'); ?>"><?php the_sub_field('retailer_name'); ?></a></li>
+
+					  	<?php endwhile; ?>
+
+					  </ul>
+
+					<?php else :
+
+					    // no rows found
+
+					endif; ?>			
+
 				</div>
-
-			<?php } ?>	
-
-			</div>			
-
-
-<div style="width: 45%; float:right">
-
-			<ul class="list-plain" style="margin-top: 0;">
-
-				<?php 
-
-				// Date
-				// --------------
-
-				if (get_field('published_date')) {
-					echo '<li><strong>Published</strong>: '.get_field('published_date').'</li>';
-				}		
-
-				// Publisher name / website
-				// --------------------------
-
-				$publisher_name = get_field('publisher_name'); 
-
-				if (get_field('publisher_website')) {
-					
-					echo '<li><strong>Publisher</strong>: <a target="_blank" href="'.get_field('publisher_website').'">';
-						echo $publisher_name->name;
-					echo '</a></li>';
-
-				}	else {
-
-					echo '<li><strong>Published by</strong>: '. $publisher_name->name .'</li>';
-
-				}
-
-				// Book type
-				// ------------------
-
-				/* $terms = get_field('book_type');
-
-				if( $terms ): $numItems = count($terms); $i = 0; ?>
-
-					<li><strong>Book Type: </strong>
-
-					<?php foreach( $terms as $term ): ?>
-
-						<?php echo $term->name; if (++$i != $numItems) {echo ', ';}?>
-					
-					<?php endforeach; ?>
-
-					</li>
-
-				<?php endif; */						
-
-				// Book format
-				// ------------------
-
-				$book_format_terms = get_field('book_format');
-
-				if( $book_format_terms ): $numItems = count($book_format_terms); $i = 0; ?>
-
-					<li><strong>Book Format: </strong>
-
-					<?php foreach( $book_format_terms as $term ): ?>
-
-						<?php echo $term->name; if (++$i != $numItems) {echo ', ';}?>
-					
-					<?php endforeach; ?>
-
-					</li>
-
-				<?php endif;	
-
-				// RRP
-				// ------------------
-
-				if (get_field('rrp')) {
-					echo '<li><strong>RRP</strong>: '.get_field('rrp').'</li>';
-				}		
-
-				// ISBN numbers
-				// ------------------
-
-				if( have_rows('isbn_numbers') ):  while ( have_rows('isbn_numbers') ) : the_row(); ?>
-
-					<li><strong><?php the_sub_field('isbn_label'); ?></strong>: <?php the_sub_field('isbn_number'); ?></li>
-
-				<?php endwhile; 
-
-				else :
-
-				// no rows found
-
-				endif; ?>
-
-			</ul>
-
-
-				<?php 
-
-				// Sales links
-				// ------------------
-
-				if( have_rows('sales_links') ): ?>
-
-					<h3>Available at these locations</h3>
-
-					<ul class="list-plain">
-
-				  	<?php while ( have_rows('sales_links') ) : the_row(); ?>
-
-				    	<li><a target="_blank" href="<?php the_sub_field('retailer_link'); ?>"><?php the_sub_field('retailer_name'); ?></a></li>
-
-				  	<?php endwhile; ?>
-
-				  </ul>
-
-				<?php else :
-
-				    // no rows found
-
-				endif; ?>			
-
-			</div>
-
-
-
-
 		</div>
 	</div>
 </div>
